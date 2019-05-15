7793111c3a967110db63d607c423baf7ff093ca7
Adam Gensler <adam.gensler@calculi.com>
2019-05-15 12:05:44 -0700

    GX-3501 update package vesion

--- SharedLib/package.ps1
+++ SharedLib/package.ps1
@@ -1,4 +1,4 @@
-﻿$ver = "1.0.6"
+﻿$ver = "1.0.7"
 if ($null -ne $env:VERSION_FOR_NUGET) {
 	$ver = $env:VERSION_FOR_NUGET
 }
@@ -8,4 +8,4 @@ Write-Host "Using version $ver"
 nuget sources add -name local -Source $env:TEMP
 MSBuild.exe /restore /t:Rebuild /p:Configuration=Release
 nuget pack .\SharedLib.csproj -Prop Configuration=Release
-nuget add .\SharedLib.1.0.6.nupkg -source $env:TEMP
+nuget add .\SharedLib.1.0.7.nupkg -source $env:TEMP
