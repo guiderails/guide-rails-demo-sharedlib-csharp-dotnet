61597e7d092fb2aad63c5bd033a89a40d4983e6b
Adam Gensler <adam.gensler@calculi.com>
2019-05-20 11:22:05 -0700

    GX-3501 only include tests in Debug

--- SharedLib/package.ps1
+++ SharedLib/package.ps1
@@ -1,4 +1,4 @@
-﻿$ver = "1.0.9"
+﻿$ver = "1.0.10"
 if ($null -ne $env:VERSION_FOR_NUGET) {
 	$ver = $env:VERSION_FOR_NUGET
 }
@@ -8,4 +8,4 @@ Write-Host "Using version $ver"
 nuget sources add -name local -Source $env:TEMP
 MSBuild.exe /restore /t:Rebuild /p:Configuration=Release
 nuget pack .\SharedLib.csproj -Prop Configuration=Release
-nuget add .\SharedLib.1.0.9.nupkg -source $env:TEMP
+nuget add .\SharedLib.1.0.10.nupkg -source $env:TEMP
